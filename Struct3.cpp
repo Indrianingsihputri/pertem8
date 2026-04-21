1f17f944bc6320bdc841cd2dfcc638d8d686f078
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-04-21 09:55:57 +0700

    MENAMPILKAN DATA ALAMAT DESA DAN KOTA

--- Struct3.cpp
+++ Struct3.cpp
@@ -14,3 +14,28 @@ struct Mahasiswa
     DetailAlamat Alamat;
     int Umur;
 };
+
+
+int main()
+{
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa: ";
+    cin >> mhs.NIM;
+    cout << "Nama Mahasiswa: ";
+    cin.ignore();
+    getline(cin, mhs.Nama);
+    cout << "Alamat Mahasiswa: ";
+    cout << "\n Nama Desa: ";
+    cin >> mhs.Alamat.desa;
+    cout << "\n Nama Kota: ";
+    cin >> mhs.Alamat.kota;
+    cout << "Umur Mahasiswa: ";
+    cin >> mhs.Umur;
+
+    cout << endl;
+    cout <<"\n NIM: " << mhs.NIM;
+    cout <<"\n Nama: " << mhs.Nama;
+    cout <<"\n Alamat: " << mhs.Alamat;
+    cout <<"\n Umur: " << mhs.Umur;
+    
+}
