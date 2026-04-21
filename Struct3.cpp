2b215e38b81e31f0bba6b64e6990a8add7a788aa
“indrianingsihputri” <indrianingsihputri30@gmail.com>
2026-04-21 09:54:42 +0700

    STRUCT3

---
+++ Struct3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Mahasiswa
+{
+    string NIM;
+    string Nama;
+    DetailAlamat Alamat;
+    int Umur;
+};
